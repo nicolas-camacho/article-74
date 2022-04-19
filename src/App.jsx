70980d15521b098b085973857e8b9b4b96747200
Nicolas Camacho <nicolas.camacho@zemoga.com>
2022-04-19 11:12:09 -0500

    :tada: Project started

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-green-300">
+      <h1 className=' m-8'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
